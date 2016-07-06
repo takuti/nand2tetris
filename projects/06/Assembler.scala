c989e8171bf4e5615ad808f8ae70dda295083891
Takuya Kitazawa <k.takuti@gmail.com>
2016-07-06 23:01:50 +0900

    make functions

--- projects/06/Assembler.scala
+++ projects/06/Assembler.scala
@@ -4,14 +4,10 @@ import assembler.{CommandType,A_COMMAND,C_COMMAND,L_COMMAND}
 import assembler.{Parser,Code,SymbolTable}
 
 object Assembler {
-  def main(args: Array[String]): Unit = {
-    if (args.size == 0) {
-      System.err.println("You need to specify a .asm file.")
-      System.exit(1)
-    }
-
-    // initialize symbol table with pre-defined symbols
+  def initializeSymbolTable(): SymbolTable = {
     val st = new SymbolTable
+
+    // pre-defined symbols
     st.addEntry("SP", 0)
     st.addEntry("LCL", 1)
     st.addEntry("ARG", 2)
@@ -21,10 +17,14 @@ object Assembler {
     st.addEntry("SCREEN", 16384)
     st.addEntry("KBD", 24576)
 
-    // first pass for symbol table construction
-    var parser = new Parser(args{0})
+    st
+  }
+
+  def firstPass(st: SymbolTable, filename: String) = {
+    val parser = new Parser(filename)
     var address = 0
 
+    // symbol table construction
     while (parser.hasMoreCommands()) {
       parser.advance()
 
@@ -35,45 +35,66 @@ object Assembler {
         else st.addEntry(parser.symbol(), address) // L_COMMAND (Xxx)
       }
     }
+  }
 
-    // open a writer for an output .hack file
-    val pattern = "(.*?)\\.asm".r
-    val output_filename = args{0} match {
-      case pattern(name) => name + ".hack"
-      case _             => System.exit(1)
-    }
-    val writer = new PrintWriter(output_filename.toString)
+  def secondPass(st: SymbolTable, in_filename: String, out_filename: String) = {
+    val writer = new PrintWriter(out_filename)
 
-    // second pass for parsing with new parser
-    parser = new Parser(args{0})
+    val parser = new Parser(in_filename)
     val code = new Code
     var ram_address = 16
 
+    // parsing
     while (parser.hasMoreCommands()) {
       parser.advance()
 
       if (!parser.command.isEmpty()) {
         val ctype: CommandType = parser.commandType()
-        var line = ""
 
         if (ctype == A_COMMAND) {
-          var symbol = parser.symbol()
-          if (!symbol.matches("[0-9]+")) { // @Xxx (Xxx is not numeric)
+          val symbol = parser.symbol()
+          var addr = 0
+
+          // @Xxx (Xxx is numeric, or not)
+          if (symbol.matches("[0-9]+")) {
+            addr = symbol.toInt
+          } else {
+            // add new label
             if (!st.contains(symbol)) {
               st.addEntry(symbol, ram_address)
               ram_address += 1
             }
-            symbol = st.getAddress(symbol).toString
-          }
-          line = String.format("0%15s", Integer.toBinaryString(symbol.toInt)).replace(' ', '0')
-        } else if (ctype == C_COMMAND) {
-          line = "111" + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump())
-        }
 
-        if (ctype != L_COMMAND) writer.write(line + "\n")
+            addr = st.getAddress(symbol)
+          }
+
+          writer.write(String.format("0%15s\n", Integer.toBinaryString(addr)).replace(' ', '0'))
+        } else if (ctype == C_COMMAND) {
+          writer.write("111" + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump()) + "\n")
+        }
       }
     }
 
     writer.close()
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.size == 0) {
+      System.err.println("You need to specify a .asm file.")
+      System.exit(1)
+    }
+
+    val st: SymbolTable = initializeSymbolTable()
+
+    firstPass(st, args{0})
+
+    // get an output filename corresponding to the .hack files
+    val pattern = "(.*?)\\.asm".r
+    val out_filename = args{0} match {
+      case pattern(name) => name + ".hack"
+      case _             => System.exit(1)
+    }
+
+    secondPass(st, args{0}, out_filename.toString)
   }
 }
