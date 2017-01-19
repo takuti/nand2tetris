e792fb22287837dfe9778b757960cdd36c64ccc1
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-19 15:54:18 +0900

    Simplify mutable map operations

--- projects/06/src/main/scala/assembler/SymbolTable.scala
+++ projects/06/src/main/scala/assembler/SymbolTable.scala
@@ -3,9 +3,9 @@ package assembler
 class SymbolTable {
   val table = collection.mutable.Map[String,Int]()
 
-  def addEntry(symbol: String, address: Int) = { table{symbol} = address }
+  def addEntry(symbol: String, address: Int) = table put (symbol, address)
 
-  def contains(symbol: String): Boolean = table.contains(symbol)
+  def contains(symbol: String): Boolean = table contains symbol
 
-  def getAddress(symbol: String): Int = table{symbol}
+  def getAddress(symbol: String): Int = table(symbol)
 }
